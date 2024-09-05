03077cdbf48e44069ad11ac4eef8a9c23e789822
RaulAM <raulartilesm@gmail.com>
2024-09-05 10:50:53 +0100

    Este este es el segundo commit de antes de la media manana

--- CLASE0509.js
+++ CLASE0509.js
@@ -1,1 +1,17 @@
 console.log("Mensaje inicial");
+
+console.log(0.1);
+
+console.log("Esto es un n/ un salto de línea");
+
+let frasePrimera = "Esta es la primera frase"
+let fraseSegunda = "Esta es la segunda frase "
+
+let sumaFrases = frasePrimera + " y " + fraseSegunda
+
+console.log(sumaFrases)
+
+let valor1 = true
+let valor2 = false
+
+console.log(valor1 || valor2)
